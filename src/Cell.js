037d909396b4ffd9c020aac13d662b5868c46f72
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-14 14:51:20 -0700

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -23,15 +23,3 @@ export default class Cell extends React.Component {
     }
 
 }
-
-
-/**
- *   <Cell />
-    ✓ is a correctly defined and exported React component which renders a <div> with a className of 'cell'
-    ✓ has a state key of 'color'
-    ✓ state.color is initially set to the 'value' prop passed from Matrix
-    ✓ has an event listener for clicks on the <div> (don't forget: 'onClick' in React!)
-    ✓ has an event listener that, when clicked, calls this.setState() once (make sure you aren't setting state directly, but instead using the component's 'setState' method)
-    ✓ has an event listener that, when clicked, sets state's 'color' key to a value of '#333'
-    ✓ sets the <div>'s inline style attribute to 'style={{backgroundColor: this.state.color}}'
- */
